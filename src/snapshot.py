76c49d2ef371b9cd60e9db58dd592e83ce7cc759
Idriss Neumann <idriss.neumann@comwork.io>
2022-08-09 16:47:13 +0100

    Prepare python script

---
+++ src/snapshot.py
@@ -0,0 +1,24 @@
+from google.cloud import storage
+from datetime import datetime
+from time import sleep
+from logger_utils import log_msg
+from common_utils import is_not_empty
+
+import os
+import sys
+
+gcs_client = storage.Client()
+src_bucket_name = os.environ['GCS_SRC_BUCKET_NAME']
+dest_bucket_prefix = os.environ['GCS_DEST_BUCKET_PREFIX']
+wait_time = os.getenv('WAIT_TIME')
+
+while True:
+    vdate = datetime.now()
+    
+    
+    
+    if is_not_empty(wait_time):
+        log_msg("INFO", "Waiting for : {}".format(wait_time))
+        sleep(wait_time)
+    else:
+        sys.exit()
