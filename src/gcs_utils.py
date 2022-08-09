05007d9e2bea2c5d6b3c9c2a3feab3fa12003870
Idriss Neumann <idriss.neumann@comwork.io>
2022-08-09 21:32:34 +0100

    Remove bucket if it exists

--- src/gcs_utils.py
+++ src/gcs_utils.py
@@ -2,9 +2,11 @@ from logger_utils import log_msg
 
 def find_or_create_bucket(gcs_client, name):
     try:
-        return gcs_client.get_bucket(name)
+        target_bucket = gcs_client.get_bucket(name)
+        target_bucket.delete(force=True)
     except Exception as e:
         log_msg("INFO", "Trying to create bucket {}, e = {}".format(name, e))
-        target_bucket = gcs_client.bucket(name)
-        target_bucket.create()
-        return target_bucket
+    
+    target_bucket = gcs_client.bucket(name)
+    target_bucket.create()
+    return target_bucket
